24bb937f8c099788e2ba209a6d3a7334baaa7b13
Paolo Elia <92871203+ImPaoloEliaX0@users.noreply.github.com>
2022-01-26 17:06:17 +0100

    Esercizio finito

---
+++ Dadi/main.js
@@ -0,0 +1,29 @@
+const start = document.getElementById('start');
+
+start.addEventListener('click',
+
+    function(){
+
+        const numberUsr = Math.floor(Math.random() * 6 ) + 1;
+        console.log(numberUsr);
+        
+        const numberPc = Math.floor(Math.random() * 6 ) + 1;
+        console.log(numberPc);
+        
+        const resultUsr = document.getElementById('resultUsr');
+        const resultPc = document.getElementById('resultPc');
+
+        resultUsr.innerHTML= numberUsr;
+        resultPc.innerHTML= numberPc;
+
+        if(numberUsr < numberPc){
+            alert('Il computer ha vinto!');
+        } else if (numberUsr == numberPc){
+            alert('Un pareggio, peccato prova di nuovo!')
+        } else {
+            alert('Complimenti hai vinto!');
+        }
+
+    }
+
+);
